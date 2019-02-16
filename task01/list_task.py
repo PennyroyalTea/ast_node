ee1ee0360062343bae4c9b9f1cf916b1aff6f2aa
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-16 17:44:15 +0300

    task01: clarify tests correctness; add immutability req to list_task

--- task01/list_task.py
+++ task01/list_task.py
@@ -1,6 +1,7 @@
 def remove_adjacent(lst):
     """
     Removes equal adjacent elements.
+    Does not modify the input list.
 
     >>> remove_adjacent([1, 2, 2, 3])
     [1, 2, 3]
@@ -11,6 +12,7 @@ def remove_adjacent(lst):
 def linear_merge(lst1, lst2):
     """
     Merges two sorted lists in one sorted list in linear time.
+    Does not modify input lists.
 
     >>> linear_merge([2, 4, 6], [1, 3, 5])
     [1, 2, 3, 4, 5, 6]
