df442eb2e9adf5769c238dadce0c591754436fa0
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-18 18:44:18 +0300

    task01: clarify that list_task.linear_merge is about implementing the algo

--- task01/list_task.py
+++ task01/list_task.py
@@ -13,6 +13,7 @@ def linear_merge(lst1, lst2):
     """
     Merges two sorted lists in one sorted list in linear time.
     Does not modify input lists.
+    Do not simply use heapq.merge, implement the algorithm.
 
     >>> linear_merge([2, 4, 6], [1, 3, 5])
     [1, 2, 3, 4, 5, 6]
