2e7d7ba1795e9fb7ae8db1a621efd71041013d30
Egor Suvorov <egor.suvorov@gmail.com>
2019-03-26 10:14:09 +0300

    task06/test_model.py: test that all classes can be constructed

--- task06/test_model.py
+++ task06/test_model.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
 import pytest
+from model import *
 
 
-def test_nothing():
-    assert 2 * 2 == 4
+def test_construction():
+    FunctionDefinition('fac', Function(['n'], [
+        Conditional(
+            BinaryOperation(Reference('n'), '=', Number(0)),
+            [Number(1)],
+            [
+                BinaryOperation(
+                    Reference('n'),
+                    '*',
+                    FunctionCall(Reference('fac'), [
+                        BinaryOperation(
+                            Reference('n'),
+                            '-',
+                            Number(1)
+                        )
+                    ])
+                )
+            ]
+        )
+    ]))
+    Read('n')
+    Print(
+        UnaryOperation('-', FunctionCall(Reference('fac'), [Reference('n')]))
+    )
 
 
 if __name__ == "__main__":
