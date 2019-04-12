a040d4af5351adb0552e6d282e216e8702adbf46
Boris Starkov <boris.starkow@gmail.com>
2019-04-12 11:42:27 +0300

    add printer.py with PrettyPrinter interface

---
+++ task06/printer.py
@@ -0,0 +1,34 @@
+from model import ASTNodeVisitor
+
+
+class PrettyPrinter(ASTNodeVisitor):
+    def visit_number(self, node):
+        pass
+
+    def visit_function(self, node):
+        pass
+
+    def visit_function_definition(self, node):
+        pass
+
+    def visit_conditional(self, node):
+        pass
+
+    def visit_print(self, node):
+        pass
+
+    def visit_read(self, node):
+        pass
+
+    def visit_function_call(self, node):
+        pass
+
+    def visit_reference(self, node):
+        pass
+
+    def visit_binary_operation(self, node):
+        pass
+
+    def visit_unary_operation(self, node):
+        pass
+
